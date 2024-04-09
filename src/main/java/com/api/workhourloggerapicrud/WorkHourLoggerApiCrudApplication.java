6dad8b834d4f3c2979402729405d9325d5fbdc48
Chiziane <eddybruno43@gmail.com>
2024-04-09 11:01:14 +0200

    setting um project

---
+++ src/main/java/com/api/workhourloggerapicrud/WorkHourLoggerApiCrudApplication.java
@@ -0,0 +1,13 @@
+package com.api.workhourloggerapicrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkHourLoggerApiCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkHourLoggerApiCrudApplication.class, args);
+    }
+
+}
